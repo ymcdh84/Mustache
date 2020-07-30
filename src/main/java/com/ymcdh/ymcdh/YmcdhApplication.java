89d0bcc8a2728fbc65f6df058c967b610478aa48
daehan.choi <daehan.choi@iljin.co.kr>
2020-07-30 14:28:18 +0900

    'test'

---
+++ src/main/java/com/ymcdh/ymcdh/YmcdhApplication.java
@@ -0,0 +1,13 @@
+package com.ymcdh.ymcdh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmcdhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YmcdhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ymcdh/ymcdh/YmcdhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ymcdh.ymcdh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmcdhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
